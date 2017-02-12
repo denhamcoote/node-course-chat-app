8c2aa070ec0224a3818b3882a5874d02ca326144
Denham Coote <denhamcoote@gmail.com>
2017-02-12 18:19:49 +0000

    add geolocation support

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,7 @@
 var expect = require('expect');
 
 var {generateMessage} = require('./message');
+var {generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -11,5 +12,16 @@ describe('generateMessage', () => {
     expect(message).toInclude({from, text});
     expect(message.from).toBe(from);
     expect(message.text).toBe(text);
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location message object', () => {
+    var from = 'Denham';
+    var lat = 1;
+    var lon = 1;
+    var message = generateLocationMessage(from, lat, lon);
+    expect(message.createdAt).toBeA('number');
+    expect(message.url).toBe('https://www.google.com/maps?q=1,1');
   });
 });
