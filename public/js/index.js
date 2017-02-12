a77bc509cd1bf05f040018bd359849fbd4590a7f
Denham Coote <denhamcoote@gmail.com>
2017-02-12 13:48:20 +0000

    emit message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    to: 'All',
-    text: 'Hello from socket.io client'
-  });
 });
 
 socket.on('disconnect', function () {
